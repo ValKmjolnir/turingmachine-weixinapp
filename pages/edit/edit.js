6d439ac2febf6be12d58418f6cd116b99b9fabf4
ValKmjolnir <lhk101lhk101@qq.com>
2022-03-01 17:29:36 +0800

    update

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -1,26 +1,45 @@
-// pages/index/index.js
+// pages/edit.js
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        
+
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        // let canvasBox=null;
+        // wx.createSelectorQuery()
+        //     .select("editor")
+        //     .context(function(res){
+        //         canvasBox=res.context;
+        //     })
+        //     .exec();
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
-
-    },
+    onReady: function() {
+        const query = wx.createSelectorQuery()
+        query.select('#editor')
+          .fields({ node: true, size: true })
+          .exec((res) => {
+            const canvas = res[0].node
+            const ctx = canvas.getContext('2d')
+    
+            const dpr = wx.getSystemInfoSync().pixelRatio
+            canvas.width = res[0].width * dpr
+            canvas.height = res[0].height * dpr
+            ctx.scale(dpr, dpr)
+    
+            ctx.fillRect(0, 0, 100, 100)
+          })
+      },
 
     /**
      * 生命周期函数--监听页面显示
@@ -62,11 +81,5 @@ Page({
      */
     onShareAppMessage: function () {
 
-    },
-
-    gotoEdit: function(param) {
-        wx.navigateTo({
-          url: "/pages/edit/edit",
-        });
     }
-});
+})
