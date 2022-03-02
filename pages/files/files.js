2317d7b59c7d96cc6065e20a934154e2060935f9
ValKmjolnir <lhk101lhk101@qq.com>
2022-03-02 17:32:14 +0800

    update

---
+++ pages/files/files.js
@@ -0,0 +1,86 @@
+// pages/files/files.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        fileList:[]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+        let fs=wx.getFileSystemManager();
+        fs.access({
+            path: `${wx.env.USER_DATA_PATH}/turingmachinesimulator`,
+            success(res){},
+            fail(res){
+                fs.mkdir({
+                    dirPath: `${wx.env.USER_DATA_PATH}/turingmachinesimulator`,
+                    recursive: false,
+                    success(res){},
+                    fail(err){}
+                });
+            }
+        });
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
+
+    readfile: function() {
+        let tmp=`${wx.env.USER_DATA_PATH}/turingmachinesimulator`;
+    },
+
+    writefile: function() {
+        let tmp=`${wx.env.USER_DATA_PATH}/turingmachinesimulator`;
+    }
+})
