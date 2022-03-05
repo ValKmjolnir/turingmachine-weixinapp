e8d87d7c871e34790016f4d1859b8c482a7c28ac
ValKmjolnir <lhk101lhk101@qq.com>
2022-03-05 15:41:12 +0800

    update

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -13,8 +13,9 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        let t=options.type;
         this.setData({
-            type:options.type
+            type:t
         });
         if("filename" in options)
             this.setData({
@@ -26,20 +27,22 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function() {
-        const query = wx.createSelectorQuery()
-        query.select('#editor')
-          .fields({ node: true, size: true })
-          .exec((res) => {
-            const canvas = res[0].node
-            const ctx = canvas.getContext('2d')
-    
-            const dpr = wx.getSystemInfoSync().pixelRatio
-            canvas.width = res[0].width * dpr
-            canvas.height = res[0].height * dpr
-            ctx.scale(dpr, dpr)
-    
-            ctx.fillRect(0, 0, 100, 100)
-          })
+        wx.createSelectorQuery()
+            .select('#canvas')
+            .fields({node:true,size:true})
+            .exec((res)=>{
+                const canvas=res[0].node;
+                const ctx=canvas.getContext('2d');
+
+                const dpr=wx.getSystemInfoSync().pixelRatio;
+                canvas.width=res[0].width*dpr;
+                canvas.height=res[0].height*dpr;
+                ctx.scale(dpr,dpr);
+                
+                // background fill color
+                ctx.fillStyle="#edf8fc";
+                ctx.fillRect(0,0,canvas.width,canvas.height);
+            });
       },
 
     /**
@@ -82,5 +85,24 @@ Page({
      */
     onShareAppMessage: function () {
 
+    },
+
+    drawCircle: function (e) {
+        var x=e.detail.x-e.target.offsetLeft;
+        var y=e.detail.y-e.target.offsetTop;
+        wx.createSelectorQuery()
+            .select("#canvas")
+            .fields({node:true,size:true})
+            .exec((res)=>{
+                const canvas=res[0].node;
+                const ctx=canvas.getContext('2d');
+                
+                const dpr=wx.getSystemInfoSync().pixelRatio;
+                ctx.beginPath();
+                ctx.arc(x,y,10,0,2*Math.PI);
+                ctx.fillStyle="#ffe985";
+                ctx.fill();
+                ctx.stroke();
+            });
     }
 })
--- pages/logs/index.js
+++ pages/logs/index.js
@@ -12,7 +12,7 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        
     },
 
     /**
@@ -26,7 +26,7 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        
     },
 
     /**
