4057cf4252fcc813d59caadec977e2e35c28b384
ValKmjolnir <lhk101lhk101@qq.com>
2022-03-08 19:44:06 +0800

    update

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -1,4 +1,9 @@
 // pages/edit.js
+var begin_x,begin_y,end_x,end_y;
+var canvas=null;
+var ctx=null;
+const dpr=wx.getSystemInfoSync().pixelRatio;
+
 Page({
 
     /**
@@ -7,7 +12,7 @@ Page({
     data: {
         type:null,
         filename:"untitled.json",
-        operand_type:"select",
+        operand_type:"select"
     },
 
     /**
@@ -21,30 +26,29 @@ Page({
         if("filename" in options)
             this.setData({
                 filename:options.filename
-            })
+            });
+        wx.createSelectorQuery()
+            .select('#canvas')
+            .fields({node:true,size:true})
+            .exec((res)=>{
+                canvas=res[0].node;
+                ctx=canvas.getContext('2d');
+
+                canvas.width=res[0].width*dpr;
+                canvas.height=res[0].height*dpr;
+                ctx.scale(dpr,dpr);
+                // background fill color
+                ctx.fillStyle="#edf8fc";
+                ctx.fillRect(0,0,canvas.width,canvas.height);
+            });
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function() {
-        wx.createSelectorQuery()
-            .select('#canvas')
-            .fields({node:true,size:true})
-            .exec((res)=>{
-                const canvas=res[0].node;
-                const ctx=canvas.getContext('2d');
-
-                const dpr=wx.getSystemInfoSync().pixelRatio;
-                canvas.width=res[0].width*dpr;
-                canvas.height=res[0].height*dpr;
-                ctx.scale(dpr,dpr);
-                
-                // background fill color
-                ctx.fillStyle="#edf8fc";
-                ctx.fillRect(0,0,canvas.width,canvas.height);
-            });
-      },
+        
+    },
 
     /**
      * 生命周期函数--监听页面显示
@@ -100,21 +104,44 @@ Page({
     },
 
     drawCircle: function (e) {
+        if(this.data.operand_type!="state")
+            return;
         var x=e.detail.x-e.target.offsetLeft;
         var y=e.detail.y-e.target.offsetTop;
-        wx.createSelectorQuery()
-            .select("#canvas")
-            .fields({node:true,size:true})
-            .exec((res)=>{
-                const canvas=res[0].node;
-                const ctx=canvas.getContext('2d');
-                
-                const dpr=wx.getSystemInfoSync().pixelRatio;
-                ctx.beginPath();
-                ctx.arc(x,y,10,0,2*Math.PI);
-                ctx.fillStyle="#ffe985";
-                ctx.fill();
-                ctx.stroke();
-            });
+
+        ctx.beginPath();
+        ctx.arc(x,y,10,0,2*Math.PI);
+        ctx.fillStyle="#ffe985";
+        ctx.fill();
+        ctx.stroke();
+    },
+
+    drawLine: function(e) {
+        if(this.data.operand_type!="func")
+            return;
+        end_x=e.changedTouches[0].x;
+        end_y=e.changedTouches[0].y;
+
+        ctx.beginPath();
+        ctx.moveTo(begin_x,begin_y);
+        ctx.lineTo(end_x,end_y);
+        ctx.stroke();
+    },
+
+    touchStart: function(e) {
+        begin_x=e.touches[0].x;
+        begin_y=e.touches[0].y;
+    },
+
+    touchEnd: function(e) {
+        if(this.data.operand_type!="func")
+            return;
+        end_x=e.changedTouches[0].x;
+        end_y=e.changedTouches[0].y;
+
+        ctx.beginPath();
+        ctx.moveTo(begin_x,begin_y);
+        ctx.lineTo(end_x,end_y);
+        ctx.stroke();
     }
 })
